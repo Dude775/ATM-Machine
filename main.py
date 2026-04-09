33d056e17388174d223758f6d1e03dbb2ac26dca
David Rubin <idf775@gmail.com>
2026-04-09 17:13:09 +0300

    clean up entry point

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from login import *
+import tkinter as tk
+from models import Bank
+from storage import load_data
+from ui import ATMApp
 
-start()
+bank = Bank()
+load_data(bank)
 
+root = tk.Tk()
+root.geometry('800x600')
+root.configure(bg="#152131")
 
+ATMApp(root, bank)
+root.mainloop()
